59332a8a4a11118845319fd1d45e6a2a57489a17
Remington Anton Amalanathan <remingtonanton@gmail.com>
2021-03-31 15:18:30 +0100

    Add greedy algorithm

--- maxSubArray.py
+++ maxSubArray.py
@@ -10,4 +10,19 @@ class Solution:
             dp[index] = max(dp[index-1] + value, value) 
             
         return max(dp)
-      
+     
+        #sumOf = 0
+        #minVal = 0 
+        #result = nums[0] 
+        #
+        #for i in nums:
+        #    
+        #    sumOf+= i 
+        #    
+        #   if (sumOf - minVal > result):
+        #        result = sumOf - minVal 
+        #    
+        #    if (sumOf < minVal): 
+        #        minVal = sumOf
+        #    
+        #return result 
